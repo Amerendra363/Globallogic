a77dd87b7b12eb307e76ac082f61a17aa0552c58
Bahubali0786 <amerendra.kunar@globallogic.com>
2022-09-21 16:32:35 +0530

    Final Capstone

---
+++ jwtyou/Credit-Card-Payment-main/creditcardpayment/src/main/java/com/globallogic/creditcardpayment/repositories/UserRepo.java
@@ -0,0 +1,12 @@
+package com.globallogic.creditcardpayment.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.globallogic.creditcardpayment.entity.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,String> {
+
+}
+//
